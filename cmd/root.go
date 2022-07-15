1734d13691facdf7606156b4d376cd397171ad5c
rollwagen <rollwagen@gmail.com>
2022-07-15 09:02:06 +0200

    Format help output to fix newline issue

--- cmd/root.go
+++ cmd/root.go
@@ -7,8 +7,8 @@ import (
 var rootCmd = &cobra.Command{
 	Use:   "tf-modver",
 	Short: "A tool that check and compares used vs. available terraform module versions in git repositories",
-	Long: `A tool that validates and compares used vs. available terraform module versions
-			in git repositories, specific modules hosted in Gitlab repositories`,
+	Long: `A tool that validates and compares used vs. available terraform module version
+in git repositories, specific modules hosted in Gitlab repositories`,
 }
 
 func Execute() error {
