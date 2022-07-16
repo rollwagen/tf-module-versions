b9b55f6e47d48e94cb0aa79f9a27dd7ab15dc091
rollwagen <rollwagen@gmail.com>
2022-07-16 09:02:40 +0200

    Minor refactoring + improve ouput messages

--- cmd/validate.go
+++ cmd/validate.go
@@ -49,71 +49,75 @@ func validate() {
 		zeroVersion, _ := version.NewVersion("0")
 
 		// for git references modules will be empty (vs registry referenced modules)
-		if sourceVersion == "" {
-			// get version ref from git url "...ref=1.1.1"
-			splitSource := strings.Split(m.Source, "=")
-			sourceVersion = splitSource[len(splitSource)-1]
+		if sourceVersion != "" {
+			continue
+		}
 
-			// get rid of tf specific generic git prefix, for details see
-			// https://www.terraform.io/language/modules/sources#generic-git-repository
-			u, _ := url.Parse(strings.Replace(m.Source, "git::", "", 1))
-			if u.Host == "" { // relative path modules don't have a host e.g. source = "./submodule"
-				continue
+		// get version ref from git url "...ref=1.1.1"
+		splitSource := strings.Split(m.Source, "=")
+		sourceVersion = splitSource[len(splitSource)-1]
+
+		// get rid of tf specific generic git:: prefix, for details see
+		// https://www.terraform.io/language/modules/sources#generic-git-repository
+		u, _ := url.Parse(strings.Replace(m.Source, "git::", "", 1))
+		if u.Host == "" { // path source have no version and also no host e.g. source = "./submodule"
+			continue
+		}
+
+		pathNoSuffix := strings.Replace(u.Path, ".git", "", 1)
+		gitlabProjectNamespaceName := strings.Replace(pathNoSuffix, "/", "", 1)
+		baseURL := fmt.Sprintf("https://%s/api/v4", u.Host)
+
+		gitlabClient, err := gitlab.NewClient(gitlabToken, gitlab.WithBaseURL(baseURL))
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "Error creating gitlab client: %s\n", err)
+			os.Exit(1)
+		}
+
+		tags, _, err := gitlabClient.Tags.ListTags(gitlabProjectNamespaceName, &gitlab.ListTagsOptions{})
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "Error querying host '%s'; potential auth issue; check GITLAB_TOKEN.\nDetails: %v\n", u.Host, err)
+			os.Exit(1)
+		}
+
+		var availableVersions []string
+		for _, t := range tags {
+			_, err := version.NewVersion(t.Name)
+			if err == nil {
+				availableVersions = append(availableVersions, t.Name)
 			}
+		}
 
-			pathNoSuffix := strings.Replace(u.Path, ".git", "", 1)
+		sort.Strings(availableVersions)
+		if len(availableVersions) > 0 {
+			latestVersion = availableVersions[len(availableVersions)-1]
+		}
 
-			gitlabProjectNamespaceName := strings.Replace(pathNoSuffix, "/", "", 1)
-			baseURL := fmt.Sprintf("https://%s/api/v4", u.Host)
-			gitlabClient, err := gitlab.NewClient(gitlabToken, gitlab.WithBaseURL(baseURL))
-			if err != nil {
-				_, _ = fmt.Fprintf(os.Stderr, "Can't create gitlab client: %s\n", err)
-				os.Exit(1)
-			}
-			//_, _, _ = gitlab.Projects.GetProject(gitlabProjectNamespaceName, &gitlab.GetProjectOptions{})
-			tags, _, err := gitlabClient.Tags.ListTags(gitlabProjectNamespaceName, &gitlab.ListTagsOptions{})
-			if err != nil {
-				_, _ = fmt.Fprint(os.Stderr, "Can't query gitlab; potential authentication error. Please check GITLAB_TOKEN")
-				os.Exit(1)
-			}
+		vLatest, _ := version.NewVersion(latestVersion)
+		vUsed, err := version.NewVersion(sourceVersion)
+		if err != nil {
+			log.Warn().
+				Str("module", m.Name).
+				Str("file", fmt.Sprintf("%s:%d", m.Pos.Filename, m.Pos.Line)).
+				Msg(fmt.Sprintf("ref '%s' is not a valid version string", sourceVersion))
+			vUsed, _ = version.NewVersion("0")
+		}
 
-			var availableVersions []string
-			for _, t := range tags {
-				_, err := version.NewVersion(t.Name)
-				if err == nil {
-					availableVersions = append(availableVersions, t.Name)
-				}
-			}
-
-			sort.Strings(availableVersions)
-			if len(availableVersions) > 0 {
-				latestVersion = availableVersions[len(availableVersions)-1]
-			}
-
-			vUsed, err := version.NewVersion(sourceVersion)
-			if err != nil {
-				log.Warn().
+		if vUsed.LessThan(vLatest) {
+			log.Info().
+				Str("module", m.Name).
+				Str("version_used", sourceVersion).
+				Str("version_latest", latestVersion).
+				Str("file", fmt.Sprintf("%s:%d", m.Pos.Filename, m.Pos.Line)).
+				Msg(color.New(color.FgRed).Add(color.Bold).Sprint("✖ ···>"))
+		} else if vUsed.Equal(vLatest) && !vUsed.Equal(zeroVersion) {
+			if !Quiet {
+				log.Debug().
 					Str("module", m.Name).
 					Str("file", fmt.Sprintf("%s:%d", m.Pos.Filename, m.Pos.Line)).
-					Msg(fmt.Sprintf("Can't parse version '%s'", sourceVersion))
-				vUsed, _ = version.NewVersion("0")
-			}
-			vLatest, _ := version.NewVersion(latestVersion)
-			if vUsed.LessThan(vLatest) {
-				log.Info().
-					Str("module", m.Name).
-					Str("version_used", sourceVersion).
-					Str("version_latest", latestVersion).
-					Str("file", fmt.Sprintf("%s:%d", m.Pos.Filename, m.Pos.Line)).
-					Msg(color.New(color.FgRed).Add(color.Bold).Sprint("✖ ···>"))
-			} else if vUsed.Equal(vLatest) && !vUsed.Equal(zeroVersion) {
-				if !Quiet {
-					log.Debug().
-						Str("module", m.Name).
-						Str("file", fmt.Sprintf("%s:%d", m.Pos.Filename, m.Pos.Line)).
-						Msg(color.New(color.FgGreen).Add(color.Bold).Sprint("✔ latest version in use"))
-				}
+					Msg(color.New(color.FgGreen).Add(color.Bold).Sprint("✔ latest version used"))
 			}
 		}
 	}
+	log.Debug().Msg("validation completed")
 }
