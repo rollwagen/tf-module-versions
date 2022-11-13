7315bd58f983980b07fcd353ecaab03252067151
rollwagen <rollwagen@gmail.com>
2022-11-13 12:28:11 +0100

    Introduce directory flag to select target dir

--- cmd/validate.go
+++ cmd/validate.go
@@ -5,18 +5,22 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var outputFormat string
+var (
+	outputFormat string
+	directory    string
+)
 
 var versionCmd = &cobra.Command{
 	Use:   "validate",
 	Short: "Print module version validation on stdout as logs",
 	Run: func(cmd *cobra.Command, args []string) {
-		validater.Validate(".", outputFormat, FlagVerbose)
+		validater.Validate(directory, outputFormat, FlagVerbose)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(versionCmd)
 
-	versionCmd.Flags().StringVarP(&outputFormat, "output", "o", "table", "Select output format (table or json)")
+	versionCmd.Flags().StringVarP(&outputFormat, "output", "o", "table", "Select output format (table, json, or noout)")
+	versionCmd.Flags().StringVarP(&directory, "directory", "d", ".", "Terraform code directory to validate")
 }
