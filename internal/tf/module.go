bc64e5f870f1df45e8f642ca2b4f653b22a0ef6a
rollwagen <rollwagen@gmail.com>
2022-11-13 12:32:14 +0100

    Change default undefined version value to 'nil'
    - introduce method to determine version diff (major/minor/patch)

--- internal/tf/module.go
+++ internal/tf/module.go
@@ -32,11 +32,11 @@ func (m Module) MarshalJSON() ([]byte, error) {
 	)
 }
 
-func NewModule(name string, usedVersion, availableVersion, fileName string, line int) (*Module, error) {
+func NewModule(name string, usedVersion, availableVersion, ref, fileName string, line int) (*Module, error) {
 	for _, v := range []string{usedVersion, availableVersion} {
-		_, err := version.NewVersion(v)
+		_, err := newVersion(v)
 		if err != nil {
-			return nil, fmt.Errorf("'%s' is not a valid version string: %w", v, err)
+			return nil, err
 		}
 	}
 
@@ -44,6 +44,7 @@ func NewModule(name string, usedVersion, availableVersion, fileName string, line
 		Name:             name,
 		UsedVersion:      usedVersion,
 		AvailableVersion: availableVersion,
+		GitReference:     ref,
 	}
 	m.Location.FileName = fileName
 	m.Location.Line = line
@@ -51,14 +52,57 @@ func NewModule(name string, usedVersion, availableVersion, fileName string, line
 	return &m, nil
 }
 
+func newVersion(versionAsString string) (*version.Version, error) {
+	versionToCreate := "0"
+	if versionAsString != "nil" {
+		versionToCreate = versionAsString
+	}
+	newVersion, err := version.NewVersion(versionToCreate)
+	if err != nil {
+		return nil, fmt.Errorf("'%s' is not a valid version string: %w", versionAsString, err)
+	}
+	return newVersion, nil
+}
+
 func (m Module) HasNewerVersion() bool {
 	ver := func(s string) *version.Version {
 		v, _ := version.NewVersion(s)
 		return v
 	}
-	return ver(m.UsedVersion).LessThan(ver(m.AvailableVersion))
+
+	usedVersion := "0"
+	if m.UsedVersion != "nil" {
+		usedVersion = m.UsedVersion
+	}
+
+	return ver(usedVersion).LessThan(ver(m.AvailableVersion))
 }
 
 func (m Module) HasSameVersion() bool {
 	return m.AvailableVersion == m.UsedVersion
+}
+
+func (m Module) NewerVersion() string {
+	versionLevel := []string{"MAJOR", "MINOR", "PATCH"}
+
+	if !m.HasNewerVersion() {
+		return "UNDEFINED"
+	}
+
+	used, _ := newVersion(m.UsedVersion)
+
+	available, _ := newVersion(m.AvailableVersion)
+
+	for i := range used.Segments() {
+
+		if i > 2 || (i > len(used.Segments())-1 && i > len(available.Segments())) {
+			return "UNDEFINED"
+		}
+
+		if available.Segments()[i] > used.Segments()[i] {
+			return versionLevel[i]
+		}
+	}
+
+	return "UNDEFINED"
 }
